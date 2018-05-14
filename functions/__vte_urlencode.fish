9ac9b8877ff0101b4f2dcfaa4a901cb34065b9a2
Sébastien Lerique <sl@mehho.net>
2018-05-14 09:26:57 +0200

    Fix typo: URI::Escape::XS exports uri_escape not uri_encode

--- functions/__vte_urlencode.fish
+++ functions/__vte_urlencode.fish
@@ -1,3 +1,3 @@
 function __vte_urlencode --argument-names str --description="encodes url byte-by-byte"
-  perl -e "use URI::Escape::XS qw(uri_encode); print uri_encode('$PWD')"
+  perl -e "use URI::Escape::XS qw(uri_escape); print uri_escape('$PWD')"
 end
